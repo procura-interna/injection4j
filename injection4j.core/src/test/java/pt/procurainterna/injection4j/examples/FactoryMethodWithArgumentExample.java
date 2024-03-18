780d26a023a4ea93cdd1bcf3f2c7167faad67617
procura interna <127160648+procura-interna@users.noreply.github.com>
2024-03-18 19:26:49 +0000

    formatting

--- injection4j.core/src/test/java/pt/procurainterna/injection4j/examples/FactoryMethodWithArgumentExample.java
+++ injection4j.core/src/test/java/pt/procurainterna/injection4j/examples/FactoryMethodWithArgumentExample.java
@@ -13,7 +13,8 @@ public class FactoryMethodWithArgumentExample {
   public static void main(String[] args) {
     ModuleBuilder moduleBuilder = ModuleBuilders.map();
     moduleBuilder.addValue(Integer.class, 2);
-    moduleBuilder.addInvocation(ExecutorService.class, Executors::newWorkStealingPool, Integer.class);
+    moduleBuilder.addInvocation(ExecutorService.class, Executors::newWorkStealingPool,
+        Integer.class);
 
     Module module = moduleBuilder.build();
     Provider provider = Providers.recursive(module);
